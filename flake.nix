311d85b16bf25f88142a29be09ef200737562623
Aidan <aidanzcase@gmail.com>
2025-08-19 18:47:42 -0500

    Update lockfile, use unstable

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
 
     wineSources = import "${nixpkgs-wine}/pkgs/applications/emulators/wine/sources.nix" {inherit pkgs;};
 
-    ccAA64 = pkgs.pkgsCross.ucrtAarch64.buildPackages.clang;
+    ccAA64 = pkgs.pkgsCross.ucrtAarch64.buildPackages.clang_20;
     ccX64 = pkgs.pkgsCross.mingwW64.buildPackages.gcc;
     ccX86 = pkgs.pkgsCross.mingw32.buildPackages.gcc;
 
@@ -57,24 +57,15 @@
       geckos = [];
       monos = [];
       patches = [ ./winepatch.patch ];
-      wineRelease = "unstable";
+      wineRelease = "stable";
       mainProgram = "wine";
     };
 
     wineHangover = pkgs.callPackage "${nixpkgs-wine}/pkgs/applications/emulators/wine/base.nix" (
       lib.recursiveUpdate wineBuildCfg {
         pname = "wineHangover";
-          version = "git";
-        src = pkgs.fetchFromGitLab {
-          owner = "wine";
-          repo = "wine";
-          rev = "92113c9f48b78928a838ba3d1d7ad2da3015d035";
-          domain = "gitlab.winehq.org";
-          sha256 = "sha256-/6rW/b1y6MX0hXI6qspEchg1H7T/Vy7c3FuHDlONB9Y=";
-        }; 
-        #src = 
-        ##version = wineSources.unstable.version;
-        #src = wineSources.unstable;
+        version = wineSources.unstable.version;
+        src = wineSources.unstable;
       }
     );
   in {
