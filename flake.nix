bf4a480e84f9152c9b7413c14772b54b30ce6d8c
Astavie <astavie@pm.me>
2025-09-17 10:18:04 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -36,28 +36,24 @@
 
     wineSources = import "${nixpkgs-wine}/pkgs/applications/emulators/wine/sources.nix" {inherit pkgs;};
 
-    ccAA64 = pkgs.pkgsCross.ucrtAarch64.buildPackages.llvmPackages_20.clangUseLLVM;
-    ccX64 = pkgs.pkgsCross.mingwW64.buildPackages.gcc;
-    ccX86 = pkgs.pkgsCross.mingw32.buildPackages.gcc;
+    ccAA64 = pkgs.pkgsCross.ucrtAarch64.buildPackages.clang_20;
 
     wineBuildCfg = {
       inherit supportFlags;
       configureFlags = [
         "--disable-tests"
-        "--enable-archs=aarch64,i386,x86_64"
+        "--enable-archs=aarch64"
         "--enable-win64"
       ];
       mingwGccs = [
         ccAA64
-        ccX64
-        ccX86
       ];
       platforms = ["aarch64-linux"];
       pkgArches = [pkgs];
       geckos = [];
       monos = [];
       patches = [ ./winepatch.patch ];
-      wineRelease = "stable";
+      wineRelease = "unstable";
       mainProgram = "wine";
     };
 
